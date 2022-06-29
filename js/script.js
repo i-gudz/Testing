4f7b17357589cb209d6ebc875405c2ea0f248f35
i-gudz <gudz.vania2015@gmail.com>
2022-06-29 12:14:42 +0300

    update 29.06

--- js/script.js
+++ js/script.js
@@ -17,3 +17,36 @@ function team(b, c) {
     document.getElementById(c).style.display = "block", b.currentTarget.classList.add("active")
 }
 sliderTeam[0].style.display = "block"
+
+var modal = document.getElementById('myModal');
+
+var span = document.getElementsByClassName("close")[0];
+
+
+function showModal() {
+    modal.style.display = "flex";
+    modal.style.opacity = 1
+}
+
+function hideModal() {
+    modal.style.display = "none";
+}
+
+span.onclick = function() {
+    modal.style.opacity = 0
+    setTimeout(hideModal, 400)
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+let toggle = document.querySelector("#toggle");
+let overlay = document.querySelector('#overlay')
+
+toggle.onclick = function(){
+  toggle.classList.toggle("active");
+  overlay.classList.toggle("open")
+}
